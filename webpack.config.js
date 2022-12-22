76ae78462ab03294281c5b2d5bf201552318b82b
MinKyu Tae <alsrb8442@naver.com>
2022-12-23 01:04:59 +0900

    🎉 Make TS-React-Webpack-Setting

---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+const webpackMode = process.env.NODE_ENV || "development";
+
+module.exports = {
+  mode: webpackMode,
+  entry: "./src/index.tsx",
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "[name].min.js",
+  },
+  devServer: {
+    liveReload: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: "asset/inline",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "public/index.html"),
+    }),
+    new CleanWebpackPlugin(),
+    // CopyWebpackPlugin: 그대로 복사할 파일들을 설정하는 플러그인
+    // 아래 patterns에 설정한 파일/폴더는 빌드 시 dist 폴더에 자동으로 생성됩니다.
+    // patterns에 설정한 경로에 해당 파일이 없으면 에러가 발생합니다.
+    // 사용하는 파일이나 폴더 이름이 다르다면 변경해주세요.
+    // 그대로 사용할 파일들이 없다면 CopyWebpackPlugin을 통째로 주석 처리 해주세요.
+    // new CopyWebpackPlugin({
+    //   patterns: [
+    //     { from: "./src/main.css", to: "./main.css" },
+    //     { from: "./src/images", to: "./images" },
+    //   ],
+    // }),
+  ],
+  devServer: {
+    hot: true,
+    host: "localhost",
+    port: 3001,
+  },
+};
